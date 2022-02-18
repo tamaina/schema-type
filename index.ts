3e3aed200b803786a18e509a5a2520927d97ba03
tamaina <tamaina@hotmail.co.jp>
2022-02-19 01:04:47 +0900

    clean up

--- index.ts
+++ index.ts
@@ -27,14 +27,14 @@ export interface Schema<Ctx extends Refs> extends OfSchema<Ctx> {
 	readonly optional?: boolean;
 	readonly items?: Schema<Ctx>;
 	readonly properties?: Obj<Ctx>;
-    // @ts-ignore
+	// @ts-ignore
 	readonly required?: ReadonlyArray<keyof this['properties']>;
 	readonly description?: string;
 	readonly example?: any;
 	readonly format?: string;
 	readonly ref?: keyof Ctx;
 	readonly enum?: ReadonlyArray<string>;
-    // @ts-ignore
+	// @ts-ignore
 	readonly default?: (this['type'] extends TypeStringef ? StringDefToType<this['type']> : any) | null;
 	readonly maxLength?: number;
 	readonly minLength?: number;
@@ -50,11 +50,11 @@ type NonOptionalPropertyNames<Ctx extends Refs, T extends Obj<Ctx>> = {
 
 type DefaultPropertyNames<Ctx extends Refs, T extends Obj<Ctx>> = {
 	[K in keyof T]: T[K]['default'] extends null ? K :
-		T[K]['default'] extends string ? K :
-		T[K]['default'] extends number ? K :
-		T[K]['default'] extends boolean ? K :
-		T[K]['default'] extends Record<string, unknown> ? K :
-		never
+	T[K]['default'] extends string ? K :
+	T[K]['default'] extends number ? K :
+	T[K]['default'] extends boolean ? K :
+	T[K]['default'] extends Record<string, unknown> ? K :
+	never
 }[keyof T];
 
 export type Obj<Ctx extends Refs> = Record<string, Schema<Ctx>>;
@@ -62,18 +62,18 @@ export type Obj<Ctx extends Refs> = Record<string, Schema<Ctx>>;
 export type ObjType<Ctx extends Refs, s extends Obj<Ctx>, RequiredProps extends ReadonlyArray<string>> =
 	{ -readonly [P in keyof s]?: SchemaType<Ctx, s[P]> } &
 	{ -readonly [P in OptionalPropertyNames<Ctx, s>]?: SchemaType<Ctx, s[P]> } &
-    { -readonly [P in RequiredProps[number]]: SchemaType<Ctx, s[P]> } &
+	{ -readonly [P in RequiredProps[number]]: SchemaType<Ctx, s[P]> } &
 	{ -readonly [P in NonOptionalPropertyNames<Ctx, s>]: SchemaType<Ctx, s[P]> } &
 	{ -readonly [P in DefaultPropertyNames<Ctx, s>]: SchemaType<Ctx, s[P]> };
 
 type NullOrUndefined<Ctx extends Refs, p extends Schema<Ctx>, T> =
 	p['nullable'] extends true
-		?	p['optional'] extends true
-			? (T | null | undefined)
-			: (T | null)
-		: p['optional'] extends true
-			? (T | undefined)
-			: T;
+	? p['optional'] extends true
+	? (T | null | undefined)
+	: (T | null)
+	: p['optional'] extends true
+	? (T | undefined)
+	: T;
 
 // 共用体型を交差型にする型 https://stackoverflow.com/questions/54938141/typescript-convert-union-to-intersection
 type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never;
@@ -81,38 +81,38 @@ type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
 // https://github.com/misskey-dev/misskey/pull/8144#discussion_r785287552
 // 単純にSchemaTypeDef<X>で判定するだけではダメ
 type UnionSchemaType<Ctx extends Refs, a extends readonly any[], X extends Schema<Ctx> = a[number]> = X extends any ? SchemaType<Ctx, X> : never;
-type ArrayUnion<T> = T extends any ? Array<T> : never; 
+type ArrayUnion<T> = T extends any ? Array<T> : never;
 
 export type SchemaTypeDef<Ctx extends Refs, p extends Schema<Ctx>> =
-    p['type'] extends 'null' ? null :
-    p['type'] extends 'integer' ? number :
-    p['type'] extends 'number' ? number :
-    p['type'] extends 'string' ? (
-        p['enum'] extends readonly string[] ?
-            p['enum'][number] :
-            p['format'] extends 'date-time' ? string : // Dateにする？？
-            string
-    ) :
-    p['type'] extends 'boolean' ? boolean :
-    p['type'] extends 'object' ? (
-        p['ref'] extends keyof Ctx ? Packed<Ctx, p['ref']> :
-        p['properties'] extends NonNullable<Obj<Ctx>> ? ObjType<Ctx, p['properties'], NonNullable<p['required']>> :
-        p['anyOf'] extends ReadonlyArray<Schema<Ctx>> ? UnionSchemaType<Ctx, p['anyOf']> & Partial<UnionToIntersection<UnionSchemaType<Ctx, p['anyOf']>>> :
-        p['allOf'] extends ReadonlyArray<Schema<Ctx>> ? UnionToIntersection<UnionSchemaType<Ctx, p['allOf']>> :
-        any
-    ) :
-    p['type'] extends 'array' ? (
-        p['items'] extends OfSchema<Ctx> ? (
-            p['items']['anyOf'] extends ReadonlyArray<Schema<Ctx>> ? UnionSchemaType<Ctx, NonNullable<p['items']['anyOf']>>[] :
-            p['items']['oneOf'] extends ReadonlyArray<Schema<Ctx>> ? ArrayUnion<UnionSchemaType<Ctx, NonNullable<p['items']['oneOf']>>> :
-            p['items']['allOf'] extends ReadonlyArray<Schema<Ctx>> ? UnionToIntersection<UnionSchemaType<Ctx, NonNullable<p['items']['allOf']>>>[] :
-            never
-        ) :
-        // @ts-ignore
-        p['items'] extends Schema<Ctx> ? SchemaTypeDef<Ctx, p['items']>[] :
-        any[]
-    ) :
-    p['oneOf'] extends ReadonlyArray<Schema<Ctx>> ? UnionSchemaType<Ctx, p['oneOf']> :
-    any;
+	p['type'] extends 'null' ? null :
+	p['type'] extends 'integer' ? number :
+	p['type'] extends 'number' ? number :
+	p['type'] extends 'string' ? (
+		p['enum'] extends readonly string[] ?
+		p['enum'][number] :
+		p['format'] extends 'date-time' ? string : // Dateにする？？
+		string
+	) :
+	p['type'] extends 'boolean' ? boolean :
+	p['type'] extends 'object' ? (
+		p['ref'] extends keyof Ctx ? Packed<Ctx, p['ref']> :
+		p['properties'] extends NonNullable<Obj<Ctx>> ? ObjType<Ctx, p['properties'], NonNullable<p['required']>> :
+		p['anyOf'] extends ReadonlyArray<Schema<Ctx>> ? UnionSchemaType<Ctx, p['anyOf']> & Partial<UnionToIntersection<UnionSchemaType<Ctx, p['anyOf']>>> :
+		p['allOf'] extends ReadonlyArray<Schema<Ctx>> ? UnionToIntersection<UnionSchemaType<Ctx, p['allOf']>> :
+		any
+	) :
+	p['type'] extends 'array' ? (
+		p['items'] extends OfSchema<Ctx> ? (
+			p['items']['anyOf'] extends ReadonlyArray<Schema<Ctx>> ? UnionSchemaType<Ctx, NonNullable<p['items']['anyOf']>>[] :
+			p['items']['oneOf'] extends ReadonlyArray<Schema<Ctx>> ? ArrayUnion<UnionSchemaType<Ctx, NonNullable<p['items']['oneOf']>>> :
+			p['items']['allOf'] extends ReadonlyArray<Schema<Ctx>> ? UnionToIntersection<UnionSchemaType<Ctx, NonNullable<p['items']['allOf']>>>[] :
+			never
+		) :
+		// @ts-ignore
+		p['items'] extends Schema<Ctx> ? SchemaTypeDef<Ctx, p['items']>[] :
+		any[]
+	) :
+	p['oneOf'] extends ReadonlyArray<Schema<Ctx>> ? UnionSchemaType<Ctx, p['oneOf']> :
+	any;
 
 export type SchemaType<Ctx extends Refs, p extends Schema<Ctx>> = NullOrUndefined<Ctx, p, SchemaTypeDef<Ctx, p>>;
--- test/playground.ts
+++ test/playground.ts
@@ -5,122 +5,122 @@ import { SchemaType, Packed } from 'schema-type';
  */
 
 const xs = {
-    type: 'object',
-    nullable: true,
-    required: ['a', 'c', 'o'],
-    properties: {
-        a: {
-            type: 'string',
-        },
-        b: {
-            type: 'number',
-        },
-        c: {
-            type: 'array',
-            items: {
-                type: 'object',
-                properties: {
-                    x: {
-                        type: 'string',
-                    }
-                }
-            }
-        },
-        d: {
-            type: 'boolean',
-        },
-        o: {
-            oneOf: [
-                { type: 'string' },
-                { type: 'integer' },
-            ]
-        },
-        p: {
-            type: 'object',
-            allOf: [
-                {
-                    type: 'object',
-                    required: ['foo'],
-                    properties: {
-                        foo: { type: 'string' },
-                    },
-                },
-                {
-                    type: 'object',
-                    required: ['bar'],
-                    properties: {
-                        bar: { type: 'string' },
-                    },
-                }
-            ],
-        },
-        q: {
-            type: 'object',
-            anyOf: [
-                {
-                    type: 'object',
-                    required: ['foo'],
-                    properties: {
-                        foo: { type: 'string' },
-                    },
-                },
-                {
-                    type: 'object',
-                    required: ['bar'],
-                    properties: {
-                        bar: { type: 'string' },
-                    },
-                },
-                {
-                    type: 'object',
-                    required: ['baz'],
-                    properties: {
-                        baz: { type: 'string' },
-                    },
-                }
-            ],
-        },
-        r: {
-            type: 'array',
-            items: {
-                oneOf: [
-                    {
-                        type: 'object',
-                        required: ['foo'],
-                        properties: {
-                            foo: { type: 'string' },
-                        },
-                    },
-                    {
-                        type: 'object',
-                        required: ['bar'],
-                        properties: {
-                            bar: { type: 'string' },
-                        },
-                    }
-                ]
-            }
-        }
-    },
+	type: 'object',
+	nullable: true,
+	required: ['a', 'c', 'o'],
+	properties: {
+		a: {
+			type: 'string',
+		},
+		b: {
+			type: 'number',
+		},
+		c: {
+			type: 'array',
+			items: {
+				type: 'object',
+				properties: {
+					x: {
+						type: 'string',
+					}
+				}
+			}
+		},
+		d: {
+			type: 'boolean',
+		},
+		o: {
+			oneOf: [
+				{ type: 'string' },
+				{ type: 'integer' },
+			]
+		},
+		p: {
+			type: 'object',
+			allOf: [
+				{
+					type: 'object',
+					required: ['foo'],
+					properties: {
+						foo: { type: 'string' },
+					},
+				},
+				{
+					type: 'object',
+					required: ['bar'],
+					properties: {
+						bar: { type: 'string' },
+					},
+				}
+			],
+		},
+		q: {
+			type: 'object',
+			anyOf: [
+				{
+					type: 'object',
+					required: ['foo'],
+					properties: {
+						foo: { type: 'string' },
+					},
+				},
+				{
+					type: 'object',
+					required: ['bar'],
+					properties: {
+						bar: { type: 'string' },
+					},
+				},
+				{
+					type: 'object',
+					required: ['baz'],
+					properties: {
+						baz: { type: 'string' },
+					},
+				}
+			],
+		},
+		r: {
+			type: 'array',
+			items: {
+				oneOf: [
+					{
+						type: 'object',
+						required: ['foo'],
+						properties: {
+							foo: { type: 'string' },
+						},
+					},
+					{
+						type: 'object',
+						required: ['bar'],
+						properties: {
+							bar: { type: 'string' },
+						},
+					}
+				]
+			}
+		}
+	},
 } as const;
 
 type X = SchemaType<{}, typeof xs>;
 
 const x: X = {
-    a: 'nyan',
-    b: 0,
-    c: [
-        { x: 'nyan' },
-    ],
-    d: 1,
-    o: 'any',
-    p: {
-        foo: 'nyan',
-    },
-    q: {
-        baz: 'nyan',
-    },
-    r: [ { foo: 'a' }, { bar: 'b' } ],
+	a: 'nyan',
+	b: 0,
+	c: [
+		{ x: 'nyan' },
+	],
+	d: 1,
+	o: 'any',
+	p: {
+		foo: 'nyan',
+	},
+	q: {
+		baz: 'nyan',
+	},
+	r: [{ foo: 'a' }, { bar: 'b' }],
 }
 
 /* 
@@ -128,17 +128,17 @@ const x: X = {
  */
 
 const rs = {
-    type: 'object',
-    properties: {
-        hoge: {
-            type: 'object',
-            ref: 'X'
-        },
-        d: {
-            type: 'string',
-        }
-    },
-    required: ['d'],
+	type: 'object',
+	properties: {
+		hoge: {
+			type: 'object',
+			ref: 'X'
+		},
+		d: {
+			type: 'string',
+		}
+	},
+	required: ['d'],
 } as const
 
 const ctx = { X: xs, R: rs };
@@ -146,18 +146,18 @@ const ctx = { X: xs, R: rs };
 type R = SchemaType<typeof ctx, typeof rs>;
 
 const r: R = {
-    hoge: {
-        a: 'nyan',
-        b: 0,
-        c: [
-            { x: 'nyan' },
-        ]
-    },
-    d: 'boo',
+	hoge: {
+		a: 'nyan',
+		b: 0,
+		c: [
+			{ x: 'nyan' },
+		]
+	},
+	d: 'boo',
 }
 
 const pr: Packed<typeof ctx, 'R'> = {
-    d: 'string',
+	d: 'string',
 }
 
 /* 
@@ -165,17 +165,17 @@ const pr: Packed<typeof ctx, 'R'> = {
  */
 
 const fs = {
-    type: 'object',
-    properties: {
-        f: {
-            type: 'object',
-            ref: 'F'
-        },
-        i: {
-            type: 'string',
-        }
-    },
-    required: ['i'],
+	type: 'object',
+	properties: {
+		f: {
+			type: 'object',
+			ref: 'F'
+		},
+		i: {
+			type: 'string',
+		}
+	},
+	required: ['i'],
 } as const
 
 const ftx = { F: fs };
@@ -183,11 +183,11 @@ const ftx = { F: fs };
 type F = SchemaType<typeof ftx, typeof fs>;
 
 const f: F = {
-    f: {
-        f: {
-            i: 'nyan',
-        },
-        i: 'bar',
-    },
-    i: 'foo',
+	f: {
+		f: {
+			i: 'nyan',
+		},
+		i: 'bar',
+	},
+	i: 'foo',
 }
